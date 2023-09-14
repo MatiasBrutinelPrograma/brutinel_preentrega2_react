86bc1c9617c76eedbaca85020688dcc6c2503872
Matias Brutinel <matiasbrutinelprograma@gmail.com>
2023-09-14 14:57:14 -0300

    Descripción-productos

--- src/Data.js
+++ src/Data.js
@@ -1,214 +1,89 @@
 const categories = [
-    {
-      id: 'Cocina',
-      name: 'Cocina',
-      products: [
-        {
-          id: '1',
-          name: 'Bajo mesada flotante',
-          description: 'Descripción del producto de cocina 1.',
-          price: 299.99,
-          image: '/images/cocina1.jpg',
-        },
-        {
-          id: '2',
-          name: 'Bajomesada con patas',
-          description: 'Descripción del producto de cocina 2.',
-          price: 249.99,
-          image: '/images/cocina2.jpg',
-        },
-        {
-            id: '3',
-            name: 'Alacena',
-            description: 'Descripción del producto de cocina 3.',
-            price: 249.99,
-            image: '/images/cocina3.jpg',
-          },
-          {
-            id: '4',
-            name: 'Despensero',
-            description: 'Descripción del producto de cocina 4.',
-            price: 249.99,
-            image: '/images/cocina4.jpg',
-          },
-          {
-            id: '5',
-            name: 'Bodega',
-            description: 'Descripción del producto de cocina 5.',
-            price: 249.99,
-            image: '/images/cocina5.jpg',
-          },
-        
-      ],
-    },
-    {
-      id: 'Habitacion',
-      name: 'Habitación',
-      products: [
-        {
-          id: '1',
-          name: 'Cama de 1 plaza',
-          description: 'Descripción del producto de habitación 1.',
-          price: 199.99,
-          image: '/images/habitacion1.jpg',
-        },
-        {
-          id: '2',
-          name: 'Cama de 2 plazas',
-          description: 'Descripción del producto de habitación 2.',
-          price: 349.99,
-          image: '/images/habitacion2.jpg',
-        },
-        {
-            id: '3',
-            name: 'Cucheta',
-            description: 'Descripción del producto de habitación 3.',
-            price: 199.99,
-            image: '/images/habitacion3.jpg',
-          },
-          {
-            id: '4',
-            name: 'Cómoda',
-            description: 'Descripción del producto de habitación 4.',
-            price: 349.99,
-            image: '/images/habitacion4.jpg',
-          },
-          {
-            id: '5',
-            name: 'Chifonier',
-            description: 'Descripción del producto de habitación 5.',
-            price: 349.99,
-            image: '/images/habitacion5.jpg',
-          },
-        // Agrega otros productos de la categoría Habitación
-      ],
-    },
+  {
+    id: 'Cocina',
+    name: 'Cocina',
+    products: [
+      {
+        id: '1',
+        name: 'Bajo mesada flotante',
+        description: 'Mueble de cocina elegante y funcional, ideal para organizar tus utensilios.',
+        price: 299.99,
+        image: '/images/cocina1.jpg',
+      },
+      {
+        id: '2',
+        name: 'Bajomesada con patas',
+        description: 'Mueble de cocina con diseño clásico y espacio para almacenar tus productos.',
+        price: 249.99,
+        image: '/images/cocina2.jpg',
+      },
+      {
+        id: '3',
+        name: 'Alacena',
+        description: 'Alacena espaciosa para guardar tus alimentos y utensilios de cocina.',
+        price: 249.99,
+        image: '/images/cocina3.jpg',
+      },
+      {
+        id: '4',
+        name: 'Despensero',
+        description: 'Organizador versátil para tu cocina, perfecto para mantener tus productos en orden.',
+        price: 249.99,
+        image: '/images/cocina4.jpg',
+      },
+      {
+        id: '5',
+        name: 'Bodega',
+        description: 'Mueble con espacio para tus botellas de vino y copas, una adición elegante a tu hogar.',
+        price: 249.99,
+        image: '/images/cocina5.jpg',
+      },
+    ],
+  },
+  {
+    id: 'Habitacion',
+    name: 'Habitación',
+    products: [
+      {
+        id: '1',
+        name: 'Cama de 1 plaza',
+        description: 'Cama individual perfecta para una habitación juvenil o de invitados.',
+        price: 199.99,
+        image: '/images/habitacion1.jpg',
+      },
+      {
+        id: '2',
+        name: 'Cama de 2 plazas',
+        description: 'Cama doble con un diseño cómodo y elegante para tu habitación principal.',
+        price: 349.99,
+        image: '/images/habitacion2.jpg',
+      },
+      {
+        id: '3',
+        name: 'Cucheta',
+        description: 'Cama litera ideal para habitaciones compartidas o para ahorrar espacio.',
+        price: 199.99,
+        image: '/images/habitacion3.jpg',
+      },
+      {
+        id: '4',
+        name: 'Cómoda',
+        description: 'Mueble de almacenamiento con cajones espaciosos, perfecto para tu ropa.',
+        price: 349.99,
+        image: '/images/habitacion4.jpg',
+      },
+      {
+        id: '5',
+        name: 'Chifonier',
+        description: 'Mueble alto y delgado con múltiples cajones para organizar tus pertenencias.',
+        price: 349.99,
+        image: '/images/habitacion5.jpg',
+      },
+      // Agrega otros productos de la categoría Habitación
+    ],
+  },
+];
 
-    {
-      id: 'Living',
-      name: 'Living',
-      products: [
-        {
-          id: '1',
-          name: 'Sillón de 3 cuerpos',
-          description: 'Sofá amplio y cómodo para tu sala de estar.',
-          price: 499.99,
-          image: '/images/living1.jpg',
-        },
-        {
-          id: '2',
-          name: 'Sillón de 2 cuerpos',
-          description: 'Sofá compacto y elegante para tu sala de estar.',
-          price: 399.99,
-          image: '/images/living2.jpg',
-        },
-        {
-          id: '3',
-          name: 'Mesa ratona',
-          description: 'Mesa de centro moderna para acompañar tus sofás.',
-          price: 149.99,
-          image: '/images/living3.jpg',
-        },
-        {
-          id: '4',
-          name: 'Futón',
-          description: 'Futón convertible ideal para espacios pequeños.',
-          price: 349.99,
-          image: '/images/living4.jpg',
-        },
-        {
-          id: '5',
-          name: 'Rack TV',
-          description: 'Mueble para TV con espacio de almacenamiento.',
-          price: 299.99,
-          image: '/images/living5.jpg',
-        },
-      ],
-    },
-    {
-      id: 'Comedor',
-      name: 'Comedor',
-      products: [
-        {
-          id: '1',
-          name: 'Sillas de madera',
-          description: 'Sillas de comedor de madera maciza.',
-          price: 89.99,
-          image: '/images/comedor1.jpg',
-        },
-        {
-          id: '2',
-          name: 'Sillas tapizadas',
-          description: 'Sillas de comedor con tapizado elegante.',
-          price: 99.99,
-          image: '/images/comedor2.jpg',
-        },
-        {
-          id: '3',
-          name: 'Mesa de Madera',
-          description: 'Mesa de comedor de madera resistente.',
-          price: 249.99,
-          image: '/images/comedor3.jpg',
-        },
-        {
-          id: '4',
-          name: 'Mesa de Vidrio',
-          description: 'Mesa de comedor con tapa de vidrio templado.',
-          price: 299.99,
-          image: '/images/comedor4.jpg',
-        },
-        {
-          id: '5',
-          name: 'Combo Desayunador con 2 banquetas',
-          description: 'Mueble desayunador con espacio para dos personas.',
-          price: 199.99,
-          image: '/images/comedor5.jpg',
-        },
-      ],
-    },
-    {
-      id: 'Baño',
-      name: 'Baño',
-      products: [
-        {
-          id: '1',
-          name: 'Perchero',
-          description: 'Perchero de pared para colgar toallas y ropa.',
-          price: 19.99,
-          image: '/images/baño1.jpg',
-        },
-        {
-          id: '2',
-          name: 'Vanitory flotante',
-          description: 'Mueble de baño con lavabo flotante.',
-          price: 199.99,
-          image: '/images/baño2.jpg',
-        },
-        {
-          id: '3',
-          name: 'Vanitory con patas',
-          description: 'Mueble de baño con lavabo y patas de diseño.',
-          price: 249.99,
-          image: '/images/baño3.jpg',
-        },
-        {
-          id: '4',
-          name: 'Organizador escalera',
-          description: 'Estante de baño estilo escalera para tus accesorios.',
-          price: 59.99,
-          image: '/images/baño4.jpg',
-        },
-        {
-          id: '5',
-          name: 'Toallero de madera',
-          description: 'Toallero de baño de madera natural.',
-          price: 29.99,
-          image: '/images/baño5.jpg',
-        },
-      ],
-    }
-    
-  ];
-  
-  export default categories;
+export default categories;
+
   
