40d1e7debb6b3deb350710f4ea8536f9f5943f04
Matias Brutinel <matiasbrutinelprograma@gmail.com>
2023-09-14 11:50:10 -0300

    Agregando_categorías

--- src/Data.js
+++ src/Data.js
@@ -83,7 +83,131 @@ const categories = [
         // Agrega otros productos de la categoría Habitación
       ],
     },
-    // Agrega otras categorías con productos
+
+    {
+      id: 'Living',
+      name: 'Living',
+      products: [
+        {
+          id: '1',
+          name: 'Sillón de 3 cuerpos',
+          description: 'Sofá amplio y cómodo para tu sala de estar.',
+          price: 499.99,
+          image: '/images/living1.jpg',
+        },
+        {
+          id: '2',
+          name: 'Sillón de 2 cuerpos',
+          description: 'Sofá compacto y elegante para tu sala de estar.',
+          price: 399.99,
+          image: '/images/living2.jpg',
+        },
+        {
+          id: '3',
+          name: 'Mesa ratona',
+          description: 'Mesa de centro moderna para acompañar tus sofás.',
+          price: 149.99,
+          image: '/images/living3.jpg',
+        },
+        {
+          id: '4',
+          name: 'Futón',
+          description: 'Futón convertible ideal para espacios pequeños.',
+          price: 349.99,
+          image: '/images/living4.jpg',
+        },
+        {
+          id: '5',
+          name: 'Rack TV',
+          description: 'Mueble para TV con espacio de almacenamiento.',
+          price: 299.99,
+          image: '/images/living5.jpg',
+        },
+      ],
+    },
+    {
+      id: 'Comedor',
+      name: 'Comedor',
+      products: [
+        {
+          id: '1',
+          name: 'Sillas de madera',
+          description: 'Sillas de comedor de madera maciza.',
+          price: 89.99,
+          image: '/images/comedor1.jpg',
+        },
+        {
+          id: '2',
+          name: 'Sillas tapizadas',
+          description: 'Sillas de comedor con tapizado elegante.',
+          price: 99.99,
+          image: '/images/comedor2.jpg',
+        },
+        {
+          id: '3',
+          name: 'Mesa de Madera',
+          description: 'Mesa de comedor de madera resistente.',
+          price: 249.99,
+          image: '/images/comedor3.jpg',
+        },
+        {
+          id: '4',
+          name: 'Mesa de Vidrio',
+          description: 'Mesa de comedor con tapa de vidrio templado.',
+          price: 299.99,
+          image: '/images/comedor4.jpg',
+        },
+        {
+          id: '5',
+          name: 'Combo Desayunador con 2 banquetas',
+          description: 'Mueble desayunador con espacio para dos personas.',
+          price: 199.99,
+          image: '/images/comedor5.jpg',
+        },
+      ],
+    },
+    {
+      id: 'Baño',
+      name: 'Baño',
+      products: [
+        {
+          id: '1',
+          name: 'Perchero',
+          description: 'Perchero de pared para colgar toallas y ropa.',
+          price: 19.99,
+          image: '/images/baño1.jpg',
+        },
+        {
+          id: '2',
+          name: 'Vanitory flotante',
+          description: 'Mueble de baño con lavabo flotante.',
+          price: 199.99,
+          image: '/images/baño2.jpg',
+        },
+        {
+          id: '3',
+          name: 'Vanitory con patas',
+          description: 'Mueble de baño con lavabo y patas de diseño.',
+          price: 249.99,
+          image: '/images/baño3.jpg',
+        },
+        {
+          id: '4',
+          name: 'Organizador escalera',
+          description: 'Estante de baño estilo escalera para tus accesorios.',
+          price: 59.99,
+          image: '/images/baño4.jpg',
+        },
+        {
+          id: '5',
+          name: 'Toallero de madera',
+          description: 'Toallero de baño de madera natural.',
+          price: 29.99,
+          image: '/images/baño5.jpg',
+        },
+      ],
+    }
+    
   ];
   
   export default categories;
