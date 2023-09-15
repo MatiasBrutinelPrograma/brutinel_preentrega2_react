e8e42b493b41e579b30ff58c8c97443c4f599eb5
Matias Brutinel <matiasbrutinelprograma@gmail.com>
2023-09-15 00:33:04 -0300

    ver carrito arriba y abajo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import ItemListContainer from './ItemListContainer';
 import ItemDetailContainer from './ItemDetailContainer';
